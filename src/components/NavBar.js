f952f2ae668fec308cc1c88fe4fba62c545af077
Josiel <jdlopez114@gmail.com>
2022-09-13 23:42:27 -0400

    working on DRY  navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,28 +3,21 @@ import { NavLink } from "react-router-dom";
 
 function NavBar() {
 
+    const selected = {
+      color: '#fff',
+      background: '#ff3c78',
+      borderRadius: '22px',
+      padding: '10px'
+    }
+
+    const notSelected ={ 
+      color: '#a0a0a0' 
+    }
+
     const NavBar = 
     <div className='navbar'>
-      <NavLink exact="true" to="/" style={({ isActive }) =>
-    isActive
-      ? {
-          color: '#fff',
-          background: '#ff3c78',
-          borderRadius: '22px',
-          padding: '10px',
-        }
-      : { color: '#a0a0a0' }
-  }> Home Page </NavLink>
-      <NavLink exact="true" to="/human" style={({ isActive }) =>
-    isActive
-      ? {
-          color: '#fff',
-          background: '#ff3c78',
-          borderRadius: '22px',
-          padding: '10px',
-        }
-      : { color: '#a0a0a0' }
-  }> Human </NavLink>
+      <NavLink exact="true" to="/" style={({ isActive }) => isActive ? selected: notSelected }> Home Page </NavLink>
+      <NavLink exact="true" to="/human" style={({ isActive }) => isActive ? selected : notSelected}> Human </NavLink>
       <NavLink exact="true" to="/alien" style={({ isActive }) =>
     isActive
       ? {
